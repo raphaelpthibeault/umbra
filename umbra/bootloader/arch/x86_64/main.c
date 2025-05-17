1a0b787a5e48f0f259ee5cfeef594a5e53305d2b
rpthibeault <rpthibeault@outlook.com>
2025-05-17 13:16:34 -0400

    zero bss

--- umbra/bootloader/arch/x86_64/main.c
+++ umbra/bootloader/arch/x86_64/main.c
@@ -1,6 +1,18 @@
 #include <types.h>
 #include "vga.h"
 
+void * 
+memset(void * dest, int c, long n) 
+{
+	__asm__ volatile("cld; rep stosb"
+	             : "=c"((int){0})
+	             : "D"(dest), "a"(c), "c"(n)
+	             : "flags", "memory");
+	return dest;
+}
+
+extern char _bss_start[];
+extern char _bss_end[];
 
 void __attribute__((noreturn))
 boot_main(void)
@@ -9,6 +21,8 @@ boot_main(void)
 	set_cursor_pos(0, 0);
 	putstr("\n*** UMBRA BOOTLOADER STAGE2 ***\n", COLOR_GRN, COLOR_BLK); // weird bug where first line starts with a space
 
+	memset(&_bss_start,0,(uintptr_t)&_bss_end-(uintptr_t)&_bss_start);
+	putstr("Zeroed BSS\n", COLOR_GRN, COLOR_BLK);
 
 	while (1);
 }
