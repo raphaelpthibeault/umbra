0d65352598852fdddac087036ddad3cabc083c48
rpthibeault <rpthibeault@outlook.com>
2025-05-18 14:54:09 -0400

    give boot_drive to stage2 C code

--- umbra/bootloader/arch/x86_64/main.c
+++ umbra/bootloader/arch/x86_64/main.c
@@ -11,11 +11,43 @@ memset(void *dest, int c, long n)
 	return dest;
 }
 
+char* 
+itoa(int value, char* result, int base) 
+{
+	// check that the base is valid
+	if (base < 2 || base > 36) { 
+		*result = '\0'; return result; 
+	}
+
+	char* ptr = result, *ptr1 = result, tmp_char;
+	int tmp_value;
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+	} while ( value );
+
+	// apply negative sign
+	if (tmp_value < 0) {
+		*ptr++ = '-';
+	}
+
+	*ptr-- = '\0';
+	while(ptr1 < ptr) {
+		tmp_char = *ptr;
+		*ptr--= *ptr1;
+		*ptr1++ = tmp_char;
+	}
+
+	return result;
+}
+
 extern char _bss_start[];
 extern char _bss_end[];
 
 void __attribute__((noreturn))
-boot_main(void)
+boot_main(uint8_t boot_drive)
 {
 	clearwin(COLOR_WHT, COLOR_BLK);
 	set_cursor_pos(0, 0);
@@ -23,6 +55,15 @@ boot_main(void)
 
 	memset(&_bss_start,0,(uintptr_t)&_bss_end-(uintptr_t)&_bss_start);
 	putstr("Zeroed BSS\n", COLOR_GRN, COLOR_BLK);
+
+	if (boot_drive != 0x80) {
+		putstr("[Warning] BIOS boot drive not 'C:'!\n\t boot drive: ", COLOR_GRN, COLOR_BLK); 
+		
+		char res[1];
+		itoa(boot_drive, res, 10);
+		putstr(res, COLOR_GRN, COLOR_BLK); 
+	}
+
 
 	while (1);
 }
