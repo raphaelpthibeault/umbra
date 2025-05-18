a843b49ddb4e6086966c4e410d94c7b52311b98b
rpthibeault <rpthibeault@outlook.com>
2025-05-18 15:22:00 -0400

    some readability fixes and added saving of real idt and setting of stub pro idt

---
+++ umbra/bootloader/arch/x86_64/asm.h
@@ -0,0 +1,11 @@
+#ifndef __ASM_H__
+#define __ASM_H__
+
+/* includes for assembly files for stage2 */
+
+#define STACK_OFF 0x1ff0
+#define CR0_PE_ON 0x1
+#define PROT_CSEG 0x08
+#define PROT_DSEG 0x10
+
+#endif // !__ASM_H__
