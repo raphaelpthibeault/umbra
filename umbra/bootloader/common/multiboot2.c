5fffae206d528a1387839b8b4ad3a19def9d1b6b
rpthibeault <rpthibeault@outlook.com>
2025-07-13 17:06:39 -0400

    multiboot2 validate magic and checksum

--- umbra/bootloader/common/multiboot2.c
+++ umbra/bootloader/common/multiboot2.c
@@ -41,6 +41,31 @@ multiboot2_load(disk_t *boot_disk, char *config)
 
 	serial_print("Multiboot2: Read kernel into memory\n");
 
+	struct multiboot_header *header;
+	for (size_t header_offset = 0; header_offset < MULTIBOOT_SEARCH; header_offset += MULTIBOOT_HEADER_ALIGN)
+	{
+		header = (void *)(kernel + header_offset);	
+
+		if (header->magic == MULTIBOOT2_HEADER_MAGIC)
+			break;
+	}
+	serial_print("Multiboot2: Found the header\n");
+
+	if (header->magic != MULTIBOOT2_HEADER_MAGIC)
+	{
+		serial_print("[PANIC] Multiboot2: Invalid magic number!\n");
+		while (1);
+	}
+	serial_print("Multiboot2: Magic is valid\n");
+
+	if (header->magic + header->architecture + header->header_length + header->checksum)
+	{
+		serial_print("[PANIC] Multiboot2: Invalid header checksum!\n");
+		while (1);
+	}
+	serial_print("Multiboot2: Checksum is valid\n");
+
+	/* TODO tags */
 
 	memmap_free(kernel_path, strlen(kernel_path) + 1);
 	while (1);
